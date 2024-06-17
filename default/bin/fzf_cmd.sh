b3dc2c34353782c3b8bc903fc24d69e4d96b1c9d
George Panayotov <georgep1245@gmail.com>
2024-06-17 11:41:15 -0700

    Make fzf_cmd more modular.
    Previously we could just select an alternative set of files to sync.
    This allows us to completely rewrite the find operation.

--- default/bin/fzf_cmd.sh
+++ default/bin/fzf_cmd.sh
@@ -2,6 +2,11 @@
 
 if [[ -e .fzf_paths ]]; then
     find $(cat .fzf_paths) -type f 2> /dev/null
+elif [[ -x .fzf_cmd ]]; then
+    ./.fzf_cmd
+elif [[ -e .fzf_cmd ]]; then
+    echo ".fzf_cmd not executable"
+    exit 1
 else
     find * -type f 2> /dev/null
 fi
